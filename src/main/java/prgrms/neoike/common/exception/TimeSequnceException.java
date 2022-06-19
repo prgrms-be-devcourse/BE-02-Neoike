6ea5788367546d11ec6c17ad1fbc6b150187f813
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-06-20 01:51:29 +0900

    feat: 도메인 저장에서 발생하는 에러 추가
    - InvalidDrawQuantityException: draw 추첨시 재고 0 일때 발생
    - TimeSequnceException: draw 저장시 등록 시간이 유효하지 않을때 발생

---
+++ src/main/java/prgrms/neoike/common/exception/TimeSequnceException.java
@@ -0,0 +1,7 @@
+package prgrms.neoike.common.exception;
+
+public class TimeSequnceException extends RuntimeException{
+    public TimeSequnceException(String message) {
+        super(message);
+    }
+}
