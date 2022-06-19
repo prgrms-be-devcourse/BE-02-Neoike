af0637433fa5d48afc79db8ae85a6d2135fc38a7
Gosh95 <goshk1995@gmail.com>
2022-06-19 20:55:02 +0900

    refactor: 신발 엔티티를 수정하라#19

--- src/main/java/prgrms/neoike/domain/sneaker/SneakerCodeCreator.java
+++ src/main/java/prgrms/neoike/domain/sneaker/SneakerCodeCreator.java
@@ -9,8 +9,8 @@ import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 @NoArgsConstructor(access = PRIVATE)
 public class SneakerCodeCreator {
 
-    public static String createSneakerCode(Category category) {
-        String preFix = category.name().substring(0, 2);
+    public static String createSneakerCode(MemberCategory memberCategory) {
+        String preFix = memberCategory.name().substring(0, 2);
         String postFix = randomNumeric(7);
 
         return format("{0}-{1}", preFix, postFix);
