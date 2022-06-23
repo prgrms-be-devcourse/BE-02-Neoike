2b842a3bc1f243c039eddc50234c089aa94970f6
Gosh95 <goshk1995@gmail.com>
2022-06-23 13:21:11 +0900

    refactor: 이미지 파일 validator 를 구현하고 테스트하라

--- src/main/java/prgrms/neoike/common/config/JpaAuditingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package prgrms.neoike.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfiguration {
-}
