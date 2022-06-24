011a0a27df4330e7ad53b6c3b66250d96017578f
yerimmac <yerim.lee.earth@gmail.com>
2022-06-25 02:58:05 +0900

    feat: Member의 Draw이력조회를 구현하라

--- src/test/java/prgrms/neoike/controller/MemberControllerTest.java
+++ src/test/java/prgrms/neoike/controller/MemberControllerTest.java
@@ -8,43 +8,45 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import prgrms.neoike.common.config.SecurityConfig;
-import prgrms.neoike.controller.dto.memberdto.MemberSaveRequest;
+import prgrms.neoike.common.jwt.JwtFilter;
+import prgrms.neoike.common.jwt.TokenProvider;
+import prgrms.neoike.config.SecurityApiTest;
 import prgrms.neoike.controller.dto.memberdto.MemberLoginRequest;
+import prgrms.neoike.controller.dto.memberdto.MemberSaveRequest;
 import prgrms.neoike.domain.member.CountryType;
 import prgrms.neoike.domain.member.Gender;
+import prgrms.neoike.service.CustomUserDetailService;
 import prgrms.neoike.service.MemberService;
+import prgrms.neoike.service.dto.drawticketdto.DrawTicketListResponse;
 import prgrms.neoike.service.dto.memberdto.MemberResponse;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(
-        controllers = MemberController.class,
-        excludeAutoConfiguration = SecurityAutoConfiguration.class,
-        excludeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                        classes = SecurityConfig.class)
-        })
+@WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
-class MemberControllerTest {
+class MemberControllerTest extends SecurityApiTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,6 +56,12 @@ class MemberControllerTest {
 
     @MockBean
     private MemberService memberService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @MockBean
+    private CustomUserDetailService customUserDetailService;
 
     @Test
     @WithMockUser
@@ -104,6 +112,41 @@ class MemberControllerTest {
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("인증(로그인)이 유효하면 응모정보를 가져온다")
+    void getMyDrawHistoryTest() throws Exception {
+        String email = "test@gmail.com";
+        String password = "1234!abcD";
+        UserDetails dummy = new User(email, password, List.of());
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
+        String token = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + token);
+
+        when(customUserDetailService.loadUserByUsername(email))
+                .thenReturn(dummy);
+
+        mockMvc.perform(get("/api/v1/members/history")
+                        .headers(httpHeaders)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("헤더에 토큰이 없으면 history api에 접근 할 수 없다")
+    void validateJwtTokenTest() throws Exception {
+        DrawTicketListResponse drawTicketListResponse = new DrawTicketListResponse(List.of());
+        when(memberService.getMyDrawHistory())
+                .thenReturn(drawTicketListResponse);
+
+        mockMvc.perform(get("/api/v1/members/history")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
                 .andDo(print());
     }
 }
