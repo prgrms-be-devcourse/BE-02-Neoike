91dd8a1218582d96ee297dedf403fe2842e10339
Yonghoon Lee <dldydgns53@naver.com>
2022-06-27 04:49:45 +0900

    refactor: 랜던 숫자를 만드는 코드에 예외처리를 하라

--- src/main/java/prgrms/neoike/common/util/RandomCreator.java
+++ src/main/java/prgrms/neoike/common/util/RandomCreator.java
@@ -8,6 +8,10 @@ import java.util.Set;
 @NoArgsConstructor
 public class RandomCreator {
     public static Set<Integer> noDuplication(int size, int maximumNumber) {
+        if(size > maximumNumber){
+            throw new RuntimeException();
+        }
+
         Set<Integer> duplicationRemoved = new HashSet<>();
 
         while (duplicationRemoved.size() < size) {
