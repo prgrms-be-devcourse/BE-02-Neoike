bec3a0bd8655c5f88e6968cc98e5a14b81e69289
yerimmac <yerim.lee.earth@gmail.com>
2022-06-28 14:04:45 +0900

    test: restdocs를 적용하라

--- src/test/java/prgrms/neoike/controller/MemberControllerTest.java
+++ src/test/java/prgrms/neoike/controller/MemberControllerTest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -16,8 +18,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import prgrms.neoike.common.jwt.JwtFilter;
 import prgrms.neoike.common.jwt.TokenProvider;
+import prgrms.neoike.config.ApiDocumentUtil;
 import prgrms.neoike.config.SecurityApiTest;
 import prgrms.neoike.controller.dto.memberdto.MemberSaveRequest;
 import prgrms.neoike.domain.member.CountryType;
@@ -25,21 +29,27 @@ import prgrms.neoike.domain.member.Gender;
 import prgrms.neoike.service.CustomUserDetailService;
 import prgrms.neoike.service.MemberService;
 import prgrms.neoike.service.dto.drawticketdto.DrawTicketListResponse;
+import prgrms.neoike.service.dto.drawticketdto.DrawTicketResponse;
 import prgrms.neoike.service.dto.memberdto.MemberResponse;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
 class MemberControllerTest extends SecurityApiTest {
 
     @Autowired
@@ -80,13 +90,37 @@ class MemberControllerTest extends SecurityApiTest {
                 .willReturn(new MemberResponse(1L, "test@gmail.com"));
 
 
-        mockMvc.perform(post("/api/v1/members")
+        ResultActions result = mockMvc.perform(post("/api/v1/members")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(status().isCreated())
                 .andDo(print());
+
+        result.andExpect(status().isCreated())
+                .andDo(document("member-create",
+                        ApiDocumentUtil.getDocumentRequest(),
+                        ApiDocumentUtil.getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일형식의 로그인아이디"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("birthday").type(JsonFieldType.STRING)
+                                        .attributes(key("format").value("yyyy-MM-dd hh:mm:ss"))
+                                        .description("생일"),
+                                fieldWithPath("city").type(JsonFieldType.STRING).description("거주도시명"),
+                                fieldWithPath("street").type(JsonFieldType.STRING).description("거주도로명"),
+                                fieldWithPath("zipcode").type(JsonFieldType.STRING).description("우편번호"),
+                                fieldWithPath("countryCode").type(JsonFieldType.STRING).description("휴대전화 국가코드"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("휴대전화번호"),
+                                fieldWithPath("gender").type(JsonFieldType.STRING).description("성별: MALE(남성)/FEMALE(여성)")
+                        ),
+                        responseFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("아이디"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("로그인아이디")
+                        )
+                ));
     }
 
     @Test
@@ -101,14 +135,31 @@ class MemberControllerTest extends SecurityApiTest {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.AUTHORIZATION, JwtFilter.AUTHORIZATION_TYPE + token);
 
+        DrawTicketResponse drawTicketResponse = new DrawTicketResponse(1L);
+        List<DrawTicketResponse> drawTicketResponses = new ArrayList<>();
+        drawTicketResponses.add(drawTicketResponse);
+        DrawTicketListResponse myDrawHistory = new DrawTicketListResponse(drawTicketResponses);
+
         when(customUserDetailService.loadUserByUsername(email))
                 .thenReturn(dummy);
+        when(memberService.getMyDrawHistory())
+                .thenReturn(myDrawHistory);
 
-        mockMvc.perform(get("/api/v1/members/draw-history")
+        ResultActions result = mockMvc.perform(get("/api/v1/members/draw-history")
                         .headers(httpHeaders)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
+
+        result.andExpect(status().isOk())
+                .andDo(document("member-getDraw-History",
+                        ApiDocumentUtil.getDocumentRequest(),
+                        ApiDocumentUtil.getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("drawTicketResponses").type(JsonFieldType.ARRAY).description("응모이력"),
+                                fieldWithPath("drawTicketResponses[].drawTicketId").type(JsonFieldType.NUMBER).description("응모이력 아이디")
+                        )
+                ));
     }
 
     @Test
