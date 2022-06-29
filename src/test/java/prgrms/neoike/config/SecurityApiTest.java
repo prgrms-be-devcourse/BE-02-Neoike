ba35b0a6836dba1fcc2677bde7a2e68da3f5d618
Gosh95 <goshk1995@gmail.com>
2022-06-29 17:06:31 +0900

    test: 컨트롤러 테스트 코드를 given/when/then 을 명확하게 구분하도록 수정하라

--- src/test/java/prgrms/neoike/config/SecurityApiTest.java
+++ src/test/java/prgrms/neoike/config/SecurityApiTest.java
@@ -1,9 +1,5 @@
 package prgrms.neoike.config;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -17,6 +13,12 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @AutoConfigureRestDocs
 @MockBean(JpaMetamodelMappingContext.class)
@@ -45,6 +47,7 @@ public abstract class SecurityApiTest {
                 .withHost("neoike.com")
                 .withPort(443)
             )
+            .alwaysExpect(content().contentType(APPLICATION_JSON))
             .alwaysDo(print())
             .build();
     }
